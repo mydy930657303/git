d0ed5241cd2f0ee2421dc72a926a2313e62ab72f
djc <930657303@qq.com>
2022-09-01 18:54:47 +0800

    g2

--- src/git1.java
+++ src/git1.java
@@ -1,7 +1,7 @@
 public class git1 {
     public static void main(String[] args) {
         System.out.println("args = " + "ss");
-
-
+        System.out.println("g1");
+        System.out.println("g2");
     }
 }
